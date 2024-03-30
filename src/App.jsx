19249d53b7931b5794b559966aa1d877764432e5
Mateo7991 <mateobaldoni@gmail.com>
2024-03-30 17:17:09 -0300

    Tienda

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import Navbar from "./components/navbar/Navbar";
+import Container from "./components/container/Container";
+
+function App() {
+
+  return (
+    <>
+  <Navbar/>
+  <Container greeting={"Bienvenido"}/>
+  </>
+  )
+}
+
+export default App
---
+++ src/components/container/Container.jsx
@@ -0,0 +1,12 @@
+
+function Container ({greeting}){
+    return(
+        <div>
+            <h2>
+                {greeting}
+            </h2>
+        </div>
+    )
+}
+
+export default Container
