ad5d33b5e132be43d7418244bf05ccaedd1aff30
Mateo7991 <mateobaldoni@gmail.com>
2024-04-27 20:45:03 -0300

    27-04

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,32 @@
-import Navbar from "./components/navbar/Navbar";
-import Container from "./components/container/Container";
+import { useState } from 'react';
+import { Header } from './components/Header/Header';
+import { ProductList } from './components/Products/ProductList';
 
 function App() {
+	const [allProducts, setAllProducts] = useState([]);
+	const [total, setTotal] = useState(0);
+	const [countProducts, setCountProducts] = useState(0);
 
-  return (
-    <>
-  <Navbar/>
-  <Container greeting={"Bienvenido"}/>
-  </>
-  )
+	return (
+		<>
+			<Header
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+			<ProductList
+				allProducts={allProducts}
+				setAllProducts={setAllProducts}
+				total={total}
+				setTotal={setTotal}
+				countProducts={countProducts}
+				setCountProducts={setCountProducts}
+			/>
+		</>
+	);
 }
 
-export default App
+export default App;
---
+++ src/Data.js
@@ -0,0 +1,24 @@
+export const data = [
+	{
+		id: 1,
+		img: 'https://http2.mlstatic.com/D_NQ_NP_760723-MLA46115014347_052021-O.webp',
+		nameProduct: 'Iphone 11',
+		price: 750000,
+		quantity: 1,
+	},
+	{
+		id: 2,
+		img: 'https://http2.mlstatic.com/D_NQ_NP_730290-MLU74557975371_022024-O.webp',
+		nameProduct: 'Motorola Edge 40',
+		price: 450000,
+		quantity: 1,
+	},
+	{
+		id: 3,
+		img: 'https://http2.mlstatic.com/D_NQ_NP_608851-MLA75172421062_032024-O.webp',
+		nameProduct: 'Samsung  Galaxy A15',
+		price: 500000,
+		quantity: 1,
+	},
+	// agregar mas imagenes
+];
